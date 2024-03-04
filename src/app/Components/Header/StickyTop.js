b0868eb4bb9a9ef9538cadacf2bbd75c06b87d94
hiteshsharma <11hiteshsharma@gmail.com>
2024-03-04 15:04:51 +0530

    added padding

--- src/app/Components/Header/StickyTop.js
+++ src/app/Components/Header/StickyTop.js
@@ -1,24 +1,23 @@
-"use client"
+"use client";
 
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const StickyTop = () => {
   return (
-    <Wrapper className='bg-[#3c3633] text-center decoration-[#eeedeb]'>
+    <Wrapper className="bg-[#3c3633] text-center decoration-[#eeedeb] py-2">
       avail 15% off on all orders above ₹3500, use code - PY15
     </Wrapper>
-  )
-}
-export default StickyTop
-
+  );
+};
+export default StickyTop;
 
 const Wrapper = styled.div`
-    font-size: 12px;
-    color: #eeedeb;
-    letter-spacing: .2em;
-    padding: 7px 20px 6px;
-    text-align: center;
-    text-transform: uppercase;
-    transition: opacity .75s ease;
-`
+  font-size: 12px;
+  color: #eeedeb;
+  letter-spacing: 0.2em;
+  padding: 7px 20px 6px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: opacity 0.75s ease;
+`;
